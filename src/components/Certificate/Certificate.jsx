c8ebbc80ea50d057e6e7ac7d8ff59e2370963ce2
Pritiranjan <patrapritiranjan11@gmail.com>
2025-01-31 18:03:02 +0530

    In Cretificate page Change the name of logo

--- src/components/Certificate/Certificate.jsx
+++ src/components/Certificate/Certificate.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import html2canvas from "html2canvas";
 import "./Certificate.css";
-import logo from "../../assets/hellow.png"; 
+import logo from "../../assets/Hellow.png"; 
 
 function Certificate() {
   const location = useLocation();
